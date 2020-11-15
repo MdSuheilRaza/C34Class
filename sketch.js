09b07b02c11c35c833266cf73ea9427847214f76
MdSuheilRaza <66959774+MdSuheilRaza@users.noreply.github.com>
2020-11-15 15:54:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var ball,dataBase,position;
+
+function setup(){
+    dataBase=firebase.database();
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    var MovingBallPosition = dataBase.ref('Ball/Position');
+    MovingBallPosition.on("value",readPosition,showError);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+
+
+function readPosition(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function writePosition(x,y){
+    dataBase.ref('Ball/Position').set({
+        'x':position.x+x,
+        'y':position.y+y
+    });
+    
+}
+
+function showError(){
+    console.log("Welcome")
+}
